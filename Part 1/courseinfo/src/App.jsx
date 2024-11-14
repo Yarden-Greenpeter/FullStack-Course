917fb80b5bf21d57d20562c3ad23e307fefa30d1
Yarden-Greenpeter <144014573+Yarden-Greenpeter@users.noreply.github.com>
2024-11-14 10:55:54 +0200

    Add files via upload
    solution to ex. 1.4 included in the code

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -40,25 +40,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 
   return (
     <div>
       <Header name={course} />
-      <Content names = {[part1.name, part2.name, part3.name]} exercises = {[part1.exercises, part2.exercises, part3.exercises]}/>
-      <Total count= {[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Content names = {[parts[0].name, parts[1].name, parts[2].name]} exercises = {[parts[0].exercises, parts[1].exercises, parts[2].exercises]}/>
+      <Total count= {[parts[0].exercises, parts[1].exercises, parts[2].exercises]}/>
     </div>
   )
 }
