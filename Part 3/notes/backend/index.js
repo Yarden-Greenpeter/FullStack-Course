49eb610a1640cb3ebe8e9764747ae931ab871ef1
Yarden-Greenpeter <ygreenpeter@gmail.com>
2025-08-26 16:11:42 +0300

    commit before exercises

--- Part 3/notes/backend/index.js
+++ Part 3/notes/backend/index.js
@@ -1,81 +1,58 @@
 
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note =require('./modules/note')
 
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).json({ 
-            error: `invalid resource id: ${id}` 
-        })
-    }
-})
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({ error: 'malformatted id' })
+        })
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-
+  
     if (!body.content) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
+      return response.status(400).json({ error: 'content missing' })
     }
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
+const unknownEndpoint = (request, response) => {
+response.status(404).send({ error: 'unknown endpoint' })
+}
 
-    notes = notes.concat(note)
-
-    response.json(note)
-})
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
